b1ccff6636e62cd2903c1e2916d7ee74f75c4ac1
User <madilovamairamkan@gmail.com>
2022-12-28 21:28:22 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+       // String[]array = {"Rita","Nargiza","Madina","Malika"};
+       // for(String a:array){
+          //  System.out.println("Men seni suiom" + a);
+
+        System.out.println("Hello World");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
